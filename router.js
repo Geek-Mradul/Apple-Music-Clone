32b4219fb33df94bc3287d8ccd357623cd94695f
Mradul Purohit <mradulpurohit123@gmail.com>
2024-10-14 16:54:25 +0530

    update

--- router.js
+++ router.js
@@ -1,46 +1,59 @@
+function loadPage($path) {
+    if ($path == "") return;
+    const container = document.getElementById("main-content");
+    const request = new XMLHttpRequest();
+    request.open("GET", "pages/" + $path + ".html");
+    request.send();
+    request.onload = function () {
+        if (request.status == 200) {
+            container.innerHTML = request.responseText;
+            console.log(request.responseText);
+
+        }
+    }
+}
 window.onload = function () {
-    const path = window.location.pathname.split("/");
-    switch (path[1]) {
-        case "": {
-            loadPage("home");
-            break;
-        }
-        case "new": {
-            loadPage("new");
-            break;
-        }
-        case "radio": {
-            loadPage("radio");
-            break;
-        }
-        default: {
-            loadPage("404");
-            break;
-        }
+
+    function getQueryParam(name) {
+        const urlParams = new URLSearchParams(window.location.search);
+        return urlParams.get(name);
     }
-    document.querySelectorAll(".head-nav-link-list").forEach((item) => {
-        item.addEventListener("click", function () {
-            const path = item.getAttribute("value");
-            loadPage(path);
-            if (path == "home") {
-                window.history.pushState("", "", "/");
+    const redirectPath = getQueryParam('redirect');
+    if (redirectPath) {
+        const pageName = redirectPath.startsWith('/') ? redirectPath.slice(1) : redirectPath;
+        window.history.pushState({}, '', redirectPath);
+        loadPage(pageName);
+    }
+
+    {
+        const path = window.location.pathname.split("/");
+        switch (path[1]) {
+            case "": {
+                loadPage("home");
+                break;
             }
-            window.history.pushState("", "", path);
+            case "new": {
+                loadPage("new");
+                break;
+            }
+            case "radio": {
+                loadPage("radio");
+                break;
+            }
+            default: {
+                loadPage("404");
+                break;
+            }
+        }
+        document.querySelectorAll(".head-nav-link-list").forEach((item) => {
+            item.addEventListener("click", function () {
+                const path = item.getAttribute("value");
+                loadPage(path);
+                if (path == "home") {
+                    window.history.pushState("", "", "/");
+                }
+                window.history.pushState("", "", path);
+            });
         });
-    });
-    function loadPage($path) {
-        if ($path == "") return;
-        const container = document.getElementById("main-content");
-        const request = new XMLHttpRequest();
-        request.open("GET", "pages/" + $path + ".html");
-        request.send();
-        request.onload = function () {
-            if (request.status == 200) {
-                container.innerHTML = request.responseText;
-                console.log(request.responseText);
-            }
-        }
     }
-
-
 }
